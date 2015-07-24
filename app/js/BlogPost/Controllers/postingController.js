59fb01cdd7588cfece5a735dca3042d954a663d3
Andre Dinh <dinh.andre@yahoo.com>
2015-07-24 11:54:24 -0700

    no errors!!

--- app/js/BlogPost/Controllers/postingController.js
+++ app/js/BlogPost/Controllers/postingController.js
@@ -1,38 +1,43 @@
 'use strict';
 module.exports = function(app){
-  app.controller('postingController', ['$scope','$http',function($scope, $http){
+  app.controller('postingController', ['$scope','resource',function($scope, resource){
+    
+    var articles = resource('articles');
+
     var refresh = function(){
-      $http.get('/articles').success(function(response){
+      articles.getall(function(response){
         console.log('Got some data');
         console.log(response);
         $scope.posts = response;
         $scope.post ='';
-      });
+        });
     };
-    refresh();
+
       $scope.destroy = function(id) {
     		console.log(id);
-    		$http.delete('/articles/' + id).success(function(response) {
-    			refresh();
-    		});
-    	};
-      refresh();
+    		articles.destroy(id,function(response) {
+    			$scope.refresh();
+    	   });
+       };
+
       $scope.newArticle = function(article){
         console.log("This is what you pass in "+ article);
-        $http.post('/articles', article).success(function(response){
-          refresh();
+        articles.submitform(article, function(response){
+          $scope.refresh();
         });
       };
-      refresh();
+
+      $scope.update = function (post){
+        articles.update(id, post, function(response){
+          post.edit = false;
+          $scope.refresh();
+        });
+      };
+
       $scope.edit = function (post) {
           post.editing = true;
           console.log('edit post'+ post);
       };
-      $scope.update = function (post){
-        $http.put('/articles/' + post._id, post).success(function(response){
-          post.edit = false;
-        });
-      }
-      refresh();
+
   }]);
 };
