b3a07522536a56471746307d25c3f722dd0fcd4f
Andre Dinh <dinh.andre@yahoo.com>
2015-07-27 09:57:45 -0700

    i dont rember

--- app/js/client.js
+++ app/js/client.js
@@ -3,7 +3,7 @@
 require('angular/angular');
 // require('angular-route');
 
-var blogPosting = angular.module('blogPosting', []);
+var blogPosting = angular.module('blogPosting', ['ngRoute']);
 
 
 //controler
@@ -14,3 +14,16 @@ require("./BlogPost/Controllers/postingController.js")(blogPosting);
 
 //directives
 require('./directives/newPostDirective.js')(blogPosting);
+
+module.exports = function(app) {
+	app.config(['$routeProvider', function($routeProvider) {
+		$routeProvider
+		.when('/newSubmit', {
+			templateUrl: '/templates/settings/directives/newSubmit.html',
+			controller: 'postingController'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+	}]);
+}
